6764b1538aa8e71150ed096753660bb1348dc909
Anil Neupane <anil.npn1993@gmail.com>
2018-11-13 09:21:39 +0100

    clean

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -18,30 +18,30 @@ var app = new Vue({
         setScore: function () {
             if (this.userPick === 'rock') {
                 if (this.randPick === 'paper') {
-                    console.log('Computer wins');
+                  //  console.log('Computer wins');
                     this.computerScore++;
                 } else if (this.randPick === 'scissors') {
-                    console.log('User wins');
+                  //  console.log('User wins');
                     this.userScore++;
                 } else {
-                    console.log('Draw');
+                  //  console.log('Draw');
                 }
             } else if (this.userPick === 'paper') {
                 if (this.randPick === 'rock') {
-                    console.log('User wins');
+                   // console.log('User wins');
                     this.userScore++;
                 } else if (this.randPick === 'scissors') {
-                    console.log('Computer wins');
+                    //console.log('Computer wins');
                     this.computerScore++;
                 } else {
                     console.log('Draw');
                 }
             } else {
                 if (this.randPick === 'rock') {
-                    console.log('Computer wins');
+                  //  console.log('Computer wins');
                     this.computerScore++;
                 } else if (this.randPick === 'paper') {
-                    console.log('User wins');
+                   // console.log('User wins');
                     this.userScore++;
                 } else {
                     console.log('Draw');
