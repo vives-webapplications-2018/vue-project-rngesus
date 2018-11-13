39557addd962f21343d594c46cd96ec972f35d58
Yoshi Hollez <yoshi.hollez@student.vives.be>
2018-11-13 08:52:43 +0100

    added routes for html pages and added css to scorebord

--- src/Models/Container.php
+++ src/Models/Container.php
@@ -5,7 +5,14 @@ class Container
 {
     public function container($username, $score)
     {   
-        
+        echo '<li class="collection-item">   
+            <span class="purple-text text-darken-2" >
+            ' . $username . '
+            </span>
+            </li>
+            <li class="collection-item">
+            description: ' . $score .'
+            </li>';
     }
 
 }
--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,10 @@ use Slim\Http\Response;
 
 // Routes
 
+$app->get('/', function (Request $request, Response $response, array $args) {
+    return $response->withRedirect('/index.phtml', 301);
+});
+
 $app->get('/scorebord', function (Request $request, Response $response, array $args) {
     $this->logger->info("GET '/scorebord' route");
     $data = RNGesus::all();
@@ -12,4 +16,29 @@ $app->get('/scorebord', function (Request $request, Response $response, array $a
         Container::container($RNGesus->username , $RNGesus->score);  
     }
     return $this->renderer->render($response, 'scorebord.phtml', $args);
+});
+
+$app->get('/credits', function (Request $request, Response $response, array $args) {
+    $this->logger->info("GET '/credits' route");
+    return $this->renderer->render($response, 'credits.html', $args);
+});
+
+$app->get('/instructions', function (Request $request, Response $response, array $args) {
+    $this->logger->info("GET '/instructions' route");
+    return $this->renderer->render($response, 'instructions.html', $args);
+});
+
+$app->get('/multiplayer', function (Request $request, Response $response, array $args) {
+    $this->logger->info("GET '/multiplayer' route");
+    return $this->renderer->render($response, 'multiplayer.html', $args);
+});
+
+$app->get('/play', function (Request $request, Response $response, array $args) {
+    $this->logger->info("GET '/play' route");
+    return $this->renderer->render($response, 'play.html', $args);
+});
+
+$app->get('/singleplayer', function (Request $request, Response $response, array $args) {
+    $this->logger->info("GET '/singleplayer' route");
+    return $this->renderer->render($response, 'singleplayer.html', $args);
 });
