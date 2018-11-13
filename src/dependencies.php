844f9d69eb3cb6267c0c776a4a6db14da007d9a2
Yoshi Hollez <yoshi.hollez@student.vives.be>
2018-11-13 09:25:44 +0100

    added illuminate database to json

--- src/dependencies.php
+++ src/dependencies.php
@@ -17,3 +17,13 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function ($container) {
+    return $capsule;
+};
