2d58659635017d22a1ff1165846997741e1a6e09
Yoshi Hollez <yoshi.hollez@student.vives.be>
2018-11-13 08:31:44 +0100

    created database and scorebord

---
+++ src/Models/Container.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Models;
+
+class Container
+{
+    public function container($username, $score)
+    {   
+        
+    }
+
+}
--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,11 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+$app->get('/scorebord', function (Request $request, Response $response, array $args) {
+    $this->logger->info("GET '/scorebord' route");
+    $data = RNGesus::all();
+    foreach ($data as $RNGesus) {
+        Container::container($RNGesus->username , $RNGesus->score);  
+    }
+    return $this->renderer->render($response, 'scorebord.phtml', $args);
 });
