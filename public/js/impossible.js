9941865ab30fe6b55379f32c745b50f869896408
Robin Spruytte <robin.spruytte646@gmail.com>
2018-11-22 16:24:46 +0100

    refactored setScore() in impossible.js

--- public/js/impossible.js
+++ public/js/impossible.js
@@ -1,6 +1,5 @@
 /*jshint esversion: 6 */
 
-
 window.onload = () => {
     const app = new Vue({
         el: "#gameAppimp",
@@ -28,44 +27,42 @@ window.onload = () => {
                 this.setScore();
             },
             setScore() {
-                if (this.userPick === 'rock') {
+                if (this.userPick === this.aiPick) {
+                    this.result = 'draw';
+                } else if (this.userPick === 'rock') {
                     if (this.aiPick === 'paper') {
                         this.result = "Computer wins";
                         this.computerScore++;
-                    } else if (this.aiPick === 'scissors') {
+                    } else {
                         this.result = "User wins";
                         this.userScore++;
-                    } else {
-                        this.result = "Draw";
                     }
                 } else if (this.userPick === 'paper') {
                     if (this.aiPick === 'scissors') {
                         this.result = "Computer wins";
                         this.computerScore++;
-                    } else if (this.aiPick === 'rock') {
+                    } else {
                         this.result = "User wins";
                         this.userScore++;
-                    } else {
-                        this.result = "Draw";
                     }
                 } else if (this.userPick === 'scissors') {
                     if (this.aiPick === 'rock') {
                         this.result = "Computer wins";
                         this.computerScore++;
-                    } else if (this.aiPick === 'paper') {
+                    } else {
                         this.result = "User wins";
                         this.userScore++;
-                    } else {
-                        this.result = "Draw";
                     }
                 }
+
                 if (this.computerScore > this.userScore) {
                     this.winning = `Computer is winning the game with ${this.computerScore} points`;
                 } else if (this.computerScore < this.userScore) {
                     this.winning = `user is winning the game with ${this.userScore} points`;
                 } 
                 else {
-                    this.winning = "It's a draw";}
+                    this.winning = "It's a draw";
+                }
                 
                 if (this.computerScore === 10) {
                     alert("COMPUTER WINS,YOU LOSE!");
