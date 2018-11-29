0377e5fbcf341cd0d3363ecdc40d4117219223f9
Yoshi Hollez <yoshi.hollez@outlook.com>
2018-11-29 17:56:37 +0100

    fixed keychecker for mp

--- public/js/keyCheck_MP.js
+++ public/js/keyCheck_MP.js
@@ -3,11 +3,11 @@ document.addEventListener("keydown", keyDownTextField, false);
 function keyDownTextField(e) {
     var keyCode = e.keyCode;
     if (keyCode == 82) {
-        app.publish('rock_grhgihrwhbuwr');
+        publish('rock_grhgihrwhbuwr');
         alert("Rock works")
     } else if (keyCode == 80) {
-        app.publish('paper_grhgihrwhbuwr');
+        publish('paper_grhgihrwhbuwr');
     } else if (keyCode == 83) {
-        app.publish('scissors_grhgihrwhbuwr');
+        publish('scissors_grhgihrwhbuwr');
     }
 }
