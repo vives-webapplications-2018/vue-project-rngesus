d3b78afd9e868cc8dc95b93b36a1ba60d24860de
Colin Pelkmans <colin.pelkmans@gmail.com>
2018-11-30 14:32:29 +0100

    fixed enter button for chat

--- public/js/keyCheck_MP.js
+++ public/js/keyCheck_MP.js
@@ -3,17 +3,15 @@ document.addEventListener("keydown", keyDownTextField, false);
 function keyDownTextField(e) {
     var keyCode = e.keyCode;
     if (keyCode == 82) {
-        app.chooseUser('rock');
-        publish('rock_grhgihrwhbuwr');
+        chooseUser('rock');
         //alert("Rock works")
     } else if (keyCode == 80) {
-        app.chooseUser('paper');
-        publish('paper_grhgihrwhbuwr')
+        chooseUser('paper');
     } else if (keyCode == 83) {
-        app.chooseUser('scissors');
-        publish('scissors_grhgihrwhbuwr')
+        chooseUser('scissors');
     } else if (keyCode == 13) {
         publish();
+        e.preventDefault();
         document.getElementById('payload').value = null;
     }
 }
