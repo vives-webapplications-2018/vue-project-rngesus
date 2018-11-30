2b5e8671e44e862f0f936635f5fff5d665a91133
Yoshi Hollez <yoshi.hollez@student.vives.be>
2018-11-30 13:41:39 +0100

    fixed bug

--- public/js/keyCheck.js
+++ public/js/keyCheck.js
@@ -3,10 +3,10 @@ document.addEventListener("keydown", keyDownTextField, false);
 function keyDownTextField(e) {
     var keyCode = e.keyCode;
     if (keyCode == 82) {
-        chooseUser('rock');
+        app.choose('rock');
     } else if (keyCode == 80) {
-        chooseUser('paper');
+        app.choose('paper');
     } else if (keyCode == 83) {
-        chooseUser('scissors');
+        app.choose('scissors');
     }
 }
--- public/js/keyCheck_MP.js
+++ public/js/keyCheck_MP.js
@@ -3,12 +3,12 @@ document.addEventListener("keydown", keyDownTextField, false);
 function keyDownTextField(e) {
     var keyCode = e.keyCode;
     if (keyCode == 82) {
-        publish('rock_grhgihrwhbuwr');
+        chooseUser('rock');
         //alert("Rock works")
     } else if (keyCode == 80) {
-        publish('paper_grhgihrwhbuwr');
+        chooseUser('paper');
     } else if (keyCode == 83) {
-        publish('scissors_grhgihrwhbuwr');
+        chooseUser('scissors');
     } else if (keyCode == 13) {
         publish();
         document.getElementById('payload').value = '';
